69409e2d3d7bc29322e2ec8fdf5a4f6be12c8654
Paula Cano <paulacanoroman@gmail.com>
2024-01-10 11:23:10 +0100

    Ex: desestructuración de dos funciones

--- global.js
+++ global.js
@@ -1,3 +1,4 @@
-const { saludar } = require("./saludos.js"); //importar modulo, se usa require en lugar de import 
+const { saludar, saludarHolaMundo } = require("./saludos.js"); //importar modulo, se usa require en lugar de import 
 
 console.log(saludar("Fede"));
+console.log(saludarHolaMundo());
