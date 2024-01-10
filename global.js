fcadd1f83b1fc7ef549581890051522a8c3bf0b0
Paula Cano <paulacanoroman@gmail.com>
2024-01-10 11:16:43 +0100

    Ex: desestructurar objeto al exportarlo

--- global.js
+++ global.js
@@ -1,4 +1,3 @@
-const saludos = require("./saludos.js"); //importar modulo, se usa require en lugar de import 
+const { saludar } = require("./saludos.js"); //importar modulo, se usa require en lugar de import 
 
-console.log(saludos.saludarHolaMundo());
-console.log(saludos.saludar("Manolo"));
+console.log(saludar("Fede"));
--- saludos.js
+++ saludos.js
@@ -7,14 +7,8 @@ function saludarHolaMundo(){
     return`Hola Mundo`
 }
 
-//Exportar modulo nombreQueLeDasAlObjeto = nombreFuncionExportar. Estos nombres son independientes aunque suelen tener el mismo nombre.
-// module.exports.saludar = saludar;
-// module.exports.saludarHolaMundo = saludarHolaMundo;
 
-// Otra forma de exportar [COMO OBJETO]: 
 module.exports = {
     saludar: saludar,
     saludarHolaMundo : saludarHolaMundo
 }
-
-// console.log(module.export);
