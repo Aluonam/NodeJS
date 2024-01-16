812e5d7fbe2f63afaa2957c29a66cb057a58f576
Paula Cano <paulacanoroman@gmail.com>
2024-01-16 11:25:17 +0100

    feat: arguments access console.log(process.args[2])

--- moduloProcess.js
+++ moduloProcess.js
@@ -1,2 +1,12 @@
 
 console.log(process)
+
+console.log(process.env)
+
+//Acceso a argumentos del terminal:
+console.log(process.argv);
+
+//indice: [node, moduloProcess, otro1, otro2]
+//indice: [  0,       1,          2,     3  ]
+
+console.log(process.argv[2]);
