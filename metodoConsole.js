6768544178701ff6d004a3343ecc9ca91de4ba41
Paula Cano <paulacanoroman@gmail.com>
2024-01-16 11:01:50 +0100

    feat: descriptive error con new Error

--- metodoConsole.js
+++ metodoConsole.js
@@ -4,3 +4,7 @@ console.log('Hola MUNDO');
 console.warn('Hay un error');
 
 console.error('Hay otro error');
+
+
+// mensaje más descriptivo de error, incluye la linea y más info en el terminal
+console.error(new Error('Ocurrió un error'));
