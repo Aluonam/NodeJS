2f6993886a0a86e45d3b003821ffb678868837d4
Paula Cano <paulacanoroman@gmail.com>
2024-01-16 12:14:57 +0100

    feat: setInterval()

---
+++ moduloSetInterval.js
@@ -0,0 +1,9 @@
+// Se ejecuta infinitamente en bucle cada ciertos milisegundos.
+
+// setInterval(funcion, intervalo, arg1, arg2)
+
+function mostrarTema(tema){
+    console.log(`Estoy aprendiendo ${tema}` )
+}
+
+setInterval(mostrarTema, 1500, 'nodeJS')
