c0aff379ae049d883a30276d56fe0333cafae38e
Paula Cano <paulacanoroman@gmail.com>
2024-01-16 11:40:32 +0100

    feat: import and show type operative system

---
+++ moduloOS.js
@@ -0,0 +1,6 @@
+// El modulo OS permite obtener información del sistema operativo.
+// importar modulo OS
+const os = require('os');
+
+//nombre del módulo (os) y nombre de la función (type())
+console.log(os.type())
